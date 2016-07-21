09c1452a807577bb321809bed338ab961cb12e5e
Trevor Vaughan <tvaughan@onyxpoint.com>
2016-07-21 11:08:38 -0400

    (SIMP-1275) Validate compliance_markup mapping
    * Ported to new RPM build structure
    * Validated compliance_markup dependencies
    
    SIMP-1275 #comment validated compliance_markup mapping
    
    Change-Id: I9a57b024a0a04cd183e28384b3fe45a71df9b9c9

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,29 +4,21 @@ describe 'autofs' do
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
-        base_facts = facts.merge({:spec_title => description})
+        let(:facts) { facts }
+        let(:hieradata) { class_name }
 
-        describe 'base' do
-          let(:facts) { base_facts }
+        it { is_expected.to create_class('autofs') }
+        it { is_expected.to contain_class('nfs') }
+        it { is_expected.to contain_class('autofs::ldap_auth') }
+        it { is_expected.to contain_package('nfs-utils').that_comes_before('Package[autofs]') }
 
-          it { is_expected.to create_class('autofs') }
-          it { is_expected.to contain_class('nfs') }
-          it { is_expected.to contain_class('autofs::ldap_auth') }
-          it { is_expected.to contain_package('nfs-utils').that_comes_before('Package[autofs]') }
-        end
-
-        describe 'no_nfs' do
-          xfacts = base_facts.dup
-          xfacts[:spec_title] = 'autofs/no_nfs'
-          let(:facts) {xfacts}
-          it { is_expected.not_to contain_package('nfs-utils').that_comes_before('Package[autofs]') }
+        context 'no_nfs' do
+          let(:hieradata) { "#{class_name}_no_nfs" }
+          it { is_expected.not_to contain_package('nfs-utils') }
         end
 
         describe 'no_ldap_auth_conf_file' do
-          facts = base_facts.dup
-          facts[:spec_title] = 'autofs/no_ldap_auth_conf_file'
-          let(:facts) {facts}
-
+          let(:hieradata) { "#{class_name}_no_ldap_auth_conf_file" }
           it { is_expected.not_to contain_class('autofs::ldap_auth') }
         end
       end
