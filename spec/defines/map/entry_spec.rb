113f9b234bdf0efc84bc7bd35c125d0aff4298fd
Steven Pritchard <steve@sicura.us>
2024-07-10 10:21:00 -0500

    Fix tests for deprecated defined types
    On Puppet 8, [strict](https://www.puppet.com/docs/puppet/8/configuration.html#strict) is set to `error` by default.  This causes [`deprecation()`](https://forge.puppet.com/modules/puppetlabs/stdlib/reference#deprecation) to fail and output an error message.
    
    This change checks the value of `strict` in tests where the deprecated defined types are used intentionally.

--- spec/defines/map/entry_spec.rb
+++ spec/defines/map/entry_spec.rb
@@ -5,14 +5,22 @@ describe 'autofs::map::entry' do
     context "on #{os}" do
       let(:facts) { os_facts }
       let(:title) { 'wildcard-stuff' }
-      let(:params) {{
-        :target => 'foo',
-        :location => '1.2.3.4:/foo'
-      }}
+      let(:params) do
+        {
+          target: 'foo',
+          location: '1.2.3.4:/foo',
+        }
+      end
 
-      it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_concat("/etc/autofs.maps.simp.d/#{params[:target]}.map").that_notifies('Exec[autofs_reload]') }
-      it { is_expected.to contain_concat__fragment("autofs_#{params[:target]}_#{title}").with_content("*\t\t#{params[:location]}") }
+      it do
+        if Puppet[:strict] == :error
+          is_expected.to compile.and_raise_error(%r{\bautofs::map::entry is deprecated\.})
+        else
+          is_expected.to compile.with_all_deps
+          is_expected.to contain_concat("/etc/autofs.maps.simp.d/#{params[:target]}.map").that_notifies('Exec[autofs_reload]')
+          is_expected.to contain_concat__fragment("autofs_#{params[:target]}_#{title}").with_content("*\t\t#{params[:location]}")
+        end
+      end
     end
   end
 end
